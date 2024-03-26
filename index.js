08d88467b5510c583563884911bb0f0e44391330
Circuitbreaker08 <53588678+Circuitbreaker08@users.noreply.github.com>
2024-03-25 21:21:19 -0400

    0.99 baby

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const c = document.getElementById("c")
 const ctx = c.getContext("2d")
 
-c.height = window.innerHeight
-c.width = window.innerWidth
+c.height = window.innerHeight * 0.99
+c.width = window.innerWidth * 0.99
 
 ctx.fillStyle = "#ff0000"
 ctx.fillRect(0, 0, c.width, c.height)
