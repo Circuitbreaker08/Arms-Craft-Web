3c5b4dd5bf4e94fafda9d01e6d5729b780b37695
Circuitbreaker08 <53588678+Circuitbreaker08@users.noreply.github.com>
2024-03-26 15:58:44 -0400

    Added event listeners for WASD

--- index.js
+++ index.js
@@ -7,4 +7,11 @@ c.width = window.innerWidth
 ctx.fillStyle = "#ff0000"
 ctx.fillRect(0, 0, c.width, c.height)
 
+document.addEventListener("keydown", (event) => {
+    if (event.key == "w") {console.log("w")}
+    if (event.key == "a") {console.log("a")}
+    if (event.key == "s") {console.log("s")}
+    if (event.key == "d") {console.log("d")}
+ })
+
 const socket = io("https://74.83.58.123:2700")
